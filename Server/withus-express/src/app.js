dceee9d28e882d4dd00065aaf7890f5b46820994
ehdwoKIM <kinbell19@gmail.com>
2024-11-07 08:21:49 +0900

    fix: package.json 수정

--- Server/withus-express/src/app.js
+++ Server/withus-express/src/app.js
@@ -1,12 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(express.json());
+
+const mongoDBUrl = process.env.DB_URL;
+
+mongoose.connect(mongoDBUrl)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('Connection error:', err));
 
 app.get('/', (req, res) => {
     res.send('WithUs Express Server!');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-  });
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
